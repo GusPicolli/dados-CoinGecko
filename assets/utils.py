8b35af8da1d849408dd2ec5d6d83d21f0e983f00
GusPicolli <gupicollibiasi@gmail.com>
2024-08-13 13:16:20 -0300

    Updated ETL process

--- assets/utils.py
+++ assets/utils.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 import requests,json
 import pandas as pd
+import pyarrow
 
-def ingestion(event):
+path_local = 'C:/Users/gusta/Desktop/'
+
+def ingestion(event: dict):
   config_ingestion = json.load(open('./assets/config.ingestion.json'))
-  response = requests.get(config_ingestion["url"], params=config_ingestion["parametros"])
+  response = requests.get(config_ingestion["url"], params=config_ingestion["parameters"])
   data = response.json()
   df = pd.DataFrame(data)
-  return df.to_parquet()
+  df.to_parquet(f'{path_local}dados-CoinGecko/AWS/S3/RAW/coins_list_raw.parquet')
+  print("arquivo salvo em RAW")
 
-def preparation(dataframe):
+def preparation(event: dict):
   metadado = json.load(open('./assets/config.preparation.json'))
+  dataframe = pd.read_parquet(f'{path_local}dados-CoinGecko/AWS/S3/RAW/coins_list.parquet')
+
   for coluna in metadado:
     if metadado[coluna] == 'string':
       dataframe[coluna] = dataframe[coluna].astype(str)
@@ -18,4 +24,6 @@ def preparation(dataframe):
       dataframe[coluna] = dataframe[coluna].astype(float)
     if metadado == 'timestamp':
       dataframe[coluna] = datetime.strptime(dataframe[coluna], formato = "%Y-%m-%d %H:%M:%S")
-  return dataframe
+  
+  dataframe.to_parquet(f'{path_local}dados-CoinGecko/AWS/S3/WORK/coins_list_work.parquet')
+  print("arquivo salvo em WORK")
--- src/app.py
+++ src/app.py
@@ -1,15 +1,29 @@
-import requests,json
+import requests
+import json
 import datetime as dt
+import sys
+sys.path.insert(1, 'C:/Users/gusta/Desktop/dados-CoinGecko/assets')
+import utils #import ingestion,preparation
 
-def ingestion(event: dict):
+def ingestion_handler(event: dict):
   #faz a ingestao
   print("começo da ingestao as " + str(dt.datetime.now()))
-  payload = event
+  payload = utils.ingestion(event)
   print("acabou a ingestao as " + str(dt.datetime.now()))
   return payload
 
+def preparation_handler(event: dict):
+  #faz a preparation
+  print("começo da preparation as " + str(dt.datetime.now()))
+  payload = utils.preparation(event)
+  print("acabou a preparation as " + str(dt.datetime.now()))
+  return payload
+
 if __name__ == "__main__":
-  t = ingestion({
+  ingestion_handler({
     "subsource": "coingecko"
     })
-  print("Hello, World!" + str(t))
+  preparation_handler({
+    "subsource": "coingecko",
+    "path": "./AWS/S3/RAW"
+    })
