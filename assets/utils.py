caa8a88b4996783165d03d42c3b56c068c406377
GusPicolli <gupicollibiasi@gmail.com>
2024-08-18 18:14:57 -0300

    Change path

--- assets/utils.py
+++ assets/utils.py
@@ -2,28 +2,29 @@ from datetime import datetime
 import requests,json
 import pandas as pd
 import pyarrow
-
-path_local = 'C:/Users/gusta/Desktop/'
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def ingestion(event: dict):
   config_ingestion = json.load(open('./assets/config.ingestion.json'))
   response = requests.get(config_ingestion["url"], params=config_ingestion["parameters"])
   data = response.json()
   df = pd.DataFrame(data)
-  df.to_parquet(f'{path_local}dados-CoinGecko/AWS/S3/RAW/coins_list_raw.parquet')
-  print("arquivo salvo em RAW")
+  df.to_parquet('../dados-CoinGecko/AWS/S3/RAW/coins_list_raw.parquet')
+  logger.info("arquivo salvo em RAW")
 
 def preparation(event: dict):
   metadado = json.load(open('./assets/config.preparation.json'))
-  dataframe = pd.read_parquet(f'{path_local}dados-CoinGecko/AWS/S3/RAW/coins_list.parquet')
+  dataframe = pd.read_parquet('../dados-CoinGecko/AWS/S3/RAW/coins_list_raw.parquet')
 
   for coluna in metadado:
     if metadado[coluna] == 'string':
       dataframe[coluna] = dataframe[coluna].astype(str)
-    if metadado == 'double':
+    if metadado[coluna] == 'double':
       dataframe[coluna] = dataframe[coluna].astype(float)
-    if metadado == 'timestamp':
-      dataframe[coluna] = datetime.strptime(dataframe[coluna], formato = "%Y-%m-%d %H:%M:%S")
+    if metadado[coluna] == 'timestamp':
+      dataframe[coluna] = pd.to_datetime(dataframe[coluna]).dt.strftime('%Y-%m-%d %H:%M:%S')
   
-  dataframe.to_parquet(f'{path_local}dados-CoinGecko/AWS/S3/WORK/coins_list_work.parquet')
-  print("arquivo salvo em WORK")
+  dataframe.to_parquet(f'../dados-CoinGecko/AWS/S3/WORK/coins_list_work.parquet')
+  logger.info("arquivo salvo em WORK")
--- src/app.py
+++ src/app.py
@@ -3,20 +3,23 @@ import json
 import datetime as dt
 import sys
 sys.path.insert(1, 'C:/Users/gusta/Desktop/dados-CoinGecko/assets')
-import utils #import ingestion,preparation
+import utils 
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def ingestion_handler(event: dict):
   #faz a ingestao
-  print("começo da ingestao as " + str(dt.datetime.now()))
+  logger.info("começo da ingestao as " + str(dt.datetime.now()))
   payload = utils.ingestion(event)
-  print("acabou a ingestao as " + str(dt.datetime.now()))
+  logger.info("acabou a ingestao as " + str(dt.datetime.now()))
   return payload
 
 def preparation_handler(event: dict):
   #faz a preparation
-  print("começo da preparation as " + str(dt.datetime.now()))
+  logger.info("começo da preparation as " + str(dt.datetime.now()))
   payload = utils.preparation(event)
-  print("acabou a preparation as " + str(dt.datetime.now()))
+  logger.info("acabou a preparation as " + str(dt.datetime.now()))
   return payload
 
 if __name__ == "__main__":
