3ef9d7f96fd116b8b0db69d49755fea821ff28cd
Gustavo Picoll Biasi <gupicollibiasi@gmail.com>
2025-02-23 22:34:44 -0300

    Bug fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,24 +9,24 @@ provider "aws" {
 #}
 
 # Inclui as funções Lambda de ingestão
-module "lambda_ingestao" {
-  source = "./"
-  repository_url = module.ecr.repository_url
-  function_name = "coingecko_etl_ingestao"
-  variables = {
-    RAW_CONFIG = "/opt/airflow/shared/S3/RAW/"
-    RAW_PATH = "/opt/airflow/shared/S3/WORK/"
-  }
-}
-
-# Inclui as funções Lambda de preparação
-module "lambda_preparacao" {
-  source = "./"
-  repository_url = module.ecr.repository_url
-  function_name = "coingecko_etl_preparacao"
-  variables = {
-    RAW_PATH = "/opt/airflow/shared/S3/WORK/"
-    WORK_CONFIG = "/opt/airflow/dags/assets/config.preparation.json"
-    WORK_PATH = "/opt/airflow/shared/S3/WORK/"
-  }
-}
+#module "lambda_ingestao" {
+#  source = "./"
+#  repository_url = module.ecr.repository_url
+#  function_name = "coingecko_etl_ingestao"
+#  variables = {
+#    RAW_CONFIG = "/opt/airflow/shared/S3/RAW/"
+#    RAW_PATH = "/opt/airflow/shared/S3/WORK/"
+#  }
+#}
+#
+## Inclui as funções Lambda de preparação
+#module "lambda_preparacao" {
+#  source = "./"
+#  repository_url = module.ecr.repository_url
+#  function_name = "coingecko_etl_preparacao"
+#  variables = {
+#    RAW_PATH = "/opt/airflow/shared/S3/WORK/"
+#    WORK_CONFIG = "/opt/airflow/dags/assets/config.preparation.json"
+#    WORK_PATH = "/opt/airflow/shared/S3/WORK/"
+#  }
+#}
